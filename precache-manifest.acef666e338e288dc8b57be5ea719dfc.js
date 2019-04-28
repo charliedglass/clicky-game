5e3b89f2b34d88a1e10133f4558fe3626f282e4c
Charlie Glass <charliedglass@gmail.com>
2019-04-27 20:16:08 -0700

    Updates

---
+++ precache-manifest.acef666e338e288dc8b57be5ea719dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd0fe25bba9d11ed999c7188a62d08f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fe7dcc1900779f77a3c1",
+    "url": "/clicky-game/static/css/main.669e641e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "fe7dcc1900779f77a3c1",
+    "url": "/clicky-game/static/js/main.f82143fe.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
